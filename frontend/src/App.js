ed594c47ad17db02bea7b05d8fe5d47321eeaa2c
Samira-Valifakhr <samira.valifakhr80@gmail.com>
2021-11-06 14:51:07 +0100

    Header anf Footer component and react-bootstrap setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div className='App'>
-      <h1>Welcom to the Car Rent!</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcom to car offer!</h1>
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 };
