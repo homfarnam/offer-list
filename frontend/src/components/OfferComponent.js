cde9ca3c66758e5b472aabdf54a4418ee0e0a386
homfarnam <homfarnam@gmail.com>
2021-11-10 20:16:30 +0330

    Remove unused

--- frontend/src/components/OfferComponent.js
+++ frontend/src/components/OfferComponent.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
 import { Card } from 'react-bootstrap';
 
 const OfferComponent = () => {
